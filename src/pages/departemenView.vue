fb3f845593e31b1e5537d3e2bc28446f05184b73
kurniawan-rgb <71911982+kurniawan-rgb@users.noreply.github.com>
2023-12-10 16:09:01 +0800

    update Departemen

--- src/pages/departemenView.vue
+++ src/pages/departemenView.vue
@@ -10,7 +10,7 @@
     import Footer from '../components/Footer.vue';
 
     export default {
+    components: { Navbar, departemen, Footer },
     name: 'departemenView',
-    components: { Navbar, departemen, Footer }
     }
 </script>
