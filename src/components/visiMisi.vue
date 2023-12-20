829af70382bd66db1379b4c1f620be4296317fe9
andiichwannn <andiichwann@gmail.com>
2023-12-20 17:58:10 +0800

    add visimisi view

--- src/components/visiMisi.vue
+++ src/components/visiMisi.vue
@@ -6,6 +6,52 @@
                 <img src="../assets/Garis1.png" alt="" style="height: 4px; width: 320px; margin-top: 20px;">
             </div>
         </div>
+
+        <div class="mt-6 px-32">
+            <div>
+                <div class="flex flex-col items-center">
+                    <div>
+                        <div class="text-black text-center text-2xl font-semibold">Visi</div>
+                        <div class="w-72 bg-[#063d63] h-1"></div>
+                    </div>
+                    <div class="text-black text-base text-justify mt-6 font-medium">
+                        <p>Menjadi institusi unggulan dalam bidang rekayasa untuk keberlanjutan global dengan semangat budaya maritim</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center mt-12 py-2 bg-gradient-to-r from-[#063d63]/80 to-[#063d63] rounded-lg">
+                    <div>
+                        <div class="text-white text-center text-2xl font-semibold">Misi</div>
+                        <div class="w-72 bg-[#EB2629] h-1"></div>
+                    </div>
+                    <div class="text-white text-base text-center mt-6 font-medium px-16">
+                        <div class="grid grid-cols-2 gap-8 py-6">
+                            <p>Mengembangkan pendidikan, penelitian, dan pengabdian masyarakat pada bidang rekayasa dengan semangat budaya maritim.</p>
+                            <p>Menghasilkan sarjana teknik yang berpengetahuan luas, menguasai teknologi maju dan berkemampuan inovasi.</p>
+                            <p>Membangun pusat unggulan bidang rekayasa untuk pembangunan berkelanjutan</p>
+                            <p>Menyebarluaskan teknologi tepat guna, peningkatan kualitas hidup, dan mempertahankan kelestarian sumber daya alam</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center mt-12">
+                    <div>
+                        <div class="text-black text-center text-2xl font-semibold">Sasaran Pendidikan</div>
+                        <div class="w-72 bg-[#063d63] h-1"></div>
+                    </div>
+                    <ul class="grid grid-cols-2 gap-x-12 gap-y-2 text-black text-base text-justify mt-6 font-medium list-disc px-8">
+                        <li>Membentuk keseimbangan penelitian – pendidikan; melalui keterpaduan sistem pendidikan yang komprehensif dan kreatif pada strata sarjana, dan percepatan penelitian pada strata pascasarjana melalui pendidikan berbasis laboratorium (Labo-Based Education / LBE)</li>
+                        <li>Mengimplementasikan model pembelajaran LBE untuk pendidikan berorientasi penelitian.</li>
+                        <li>Membangun jaringan kemitraan internasional untuk pendidikan dan penelitian.</li>
+                        <li>Mengelola kegiatan pendidikan dan penelitian lintas disiplin ilmu.</li>
+                        <li>Menghasilkan sarjana teknik yang memenuhi kriteria lulusan pendidikan keteknikan internasional</li>
+                        <li>Menghasilkan sarjana teknik dengan kompetensi sesuai dengan kebutuhan pasar kerja.</li>
+                        <li>Menekankan pendidikan dan enelitian untuk pembangunan berkelanjutan bagi masyarakat global.</li>
+                        <li>Mengintensifkan penelitian untuk pembangunan berkelanjutan di kawasan Indonesia Timur.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
